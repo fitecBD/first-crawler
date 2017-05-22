58d3507aaf9893c1340df3f7ab462e8f227b539f
Maria <mary.romanenko@gmail.com>
2017-05-22 10:23:34 +0200

    2 3 petits changements

--- src/main/java/fitec/swag/first_updater/App.java
+++ src/main/java/fitec/swag/first_updater/App.java
@@ -37,178 +37,173 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class App {
-	private static Logger logger = LogManager.getLogger(App.class);
+    private static Logger logger = LogManager.getLogger(App.class);
+    PropertiesConfiguration config;
+    private String mongoURI = "mongodb://localhost:27017";
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+    @SuppressWarnings("rawtypes")
+    private MongoCollection outputCollection;
+    private String databaseName = "crowdfunding";
+    private String collectionName = "kickstarter";
+    private String urlBase = "https://www.kickstarter.com/discover/advanced?sort=newest&page=";
+    private ArrayList<org.bson.Document> mongoDocuments = new ArrayList<>();
+    private JSONArray mongoDocumentsJsonArray = new JSONArray();
+    private HashSet<Integer> idsProjetsCrawles = new HashSet<>();
+    private List<Exception> exceptions = new ArrayList<>();
+    private int cptProjects = 0;
 
-	private String mongoURI = "mongodb://localhost:27017";
-	private MongoClient mongoClient;
-	private MongoDatabase mongoDatabase;
-	@SuppressWarnings("rawtypes")
-	private MongoCollection outputCollection;
-	private String databaseName = "crowdfunding";
-	private String collectionName = "project";
+    public App() throws ConfigurationException {
+        super();
+        FileBasedConfigurationBuilder<PropertiesConfiguration> builder = new FileBasedConfigurationBuilder<PropertiesConfiguration>(
+                PropertiesConfiguration.class).configure(
+                new Parameters().properties().setFileName("config.properties").setThrowExceptionOnMissing(true)
+                        .setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
+                        .setIncludesAllowed(false));
+        this.config = builder.getConfiguration();
+    }
 
-	private String urlBase = "https://www.kickstarter.com/discover/advanced?sort=newest&page=";
+    public static void main(String[] args) throws ConfigurationException {
+        App app = new App();
+        app.run();
+    }
 
-	private ArrayList<org.bson.Document> mongoDocuments = new ArrayList<>();
-	private JSONArray mongoDocumentsJsonArray = new JSONArray();
-
-	private HashSet<Integer> idsProjetsCrawles = new HashSet<>();
-
-	private List<Exception> exceptions = new ArrayList<>();
-
-	private int cptProjects = 0;
-
-	PropertiesConfiguration config;
-
-	public App() throws ConfigurationException {
-		super();
-		FileBasedConfigurationBuilder<PropertiesConfiguration> builder = new FileBasedConfigurationBuilder<PropertiesConfiguration>(
-				PropertiesConfiguration.class).configure(
-						new Parameters().properties().setFileName("config.properties").setThrowExceptionOnMissing(true)
-								.setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
-								.setIncludesAllowed(false));
-		this.config = builder.getConfiguration();
-	}
-
-	public static void main(String[] args) throws ConfigurationException {
-		App app = new App();
-		app.run();
-	}
-
-	private void run() {
-		initFromProperties();
-		initMongo();
-		getIdsProjects();
-		try {
-			doUpdate();
-		} catch (Exception e) {
-			logger.error(e);
-			e.printStackTrace();
-			exceptions.add(e);
-		}
-		logger.info("coucou");
+    private void run() {
+        initFromProperties();
+        //initMongo();
+        getIdsProjects();
+        try {
+            doUpdate();
+        } catch (Exception e) {
+            logger.error(e);
+            e.printStackTrace();
+            exceptions.add(e);
+        }
+        logger.info("coucou");
         System.out.println("coucou");
-		syste
-		if (exceptions.isEmpty()) {
-		    if(!mongoDocuments.isEmpty()){
+        if (exceptions.isEmpty()) {
+            if (!mongoDocuments.isEmpty()) {
+                mongoClient = new MongoClient(new MongoClientURI(this.mongoURI));
                 outputCollection.insertMany(mongoDocuments);
-            }else {
-		        logger.info("pas de nouveau projets");
+            } else {
+                logger.info("pas de nouveau projets");
             }
-		} else {
-			logger.info("erreur-s lors de l'upadte, documents non pushés dans mongo");
-		}
-		try {
-			FileUtils.writeStringToFile(new File("update-" + getFormattedDate() + ".json"),
-					mongoDocumentsJsonArray.toString(), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			logger.error(e);
-			e.printStackTrace();
-		}
-		mongoClient.close();
-	}
+        } else {
+            logger.info("erreur-s lors de l'upadte, documents non pushés dans mongo");
+        }
+        try {
+            FileUtils.writeStringToFile(new File("update-" + getFormattedDate() + ".json"),
+                    mongoDocumentsJsonArray.toString(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            logger.error(e);
+            e.printStackTrace();
+        }
+        mongoClient.close();
+    }
 
-	private void initFromProperties() {
-		this.mongoURI = config.getString("mongo.uri");
-		this.databaseName = config.getString("mongo.database");
-		this.collectionName = config.getString("mongo.collection");
-	}
+    private void initFromProperties() {
+        this.mongoURI = config.getString("mongo.uri");
+        this.databaseName = config.getString("mongo.database");
+        this.collectionName = config.getString("mongo.collection");
+    }
 
-	private void initMongo() {
-		mongoClient = new MongoClient(new MongoClientURI(this.mongoURI));
-		mongoDatabase = mongoClient.getDatabase(databaseName);
-		outputCollection = mongoDatabase.getCollection(collectionName);
-	}
+    private void initMongo() {
+        mongoClient = new MongoClient(new MongoClientURI(this.mongoURI));
+        mongoDatabase = mongoClient.getDatabase(databaseName);
+        outputCollection = mongoDatabase.getCollection(collectionName);
+    }
 
-	private void getIdsProjects() {
-		// Construction de la requête
-		// BasicDBObject allQuery = new BasicDBObject();
-		BasicDBObject fields = new BasicDBObject();
-		fields.put("id", 1);
-		fields.put("_id", 0);
+    private void getIdsProjects() {
+        // Construction de la requête
+        // BasicDBObject allQuery = new BasicDBObject();
+        BasicDBObject fields = new BasicDBObject();
+        fields.put("id", 1);
+        fields.put("_id", 0);
 
-		// on ajoute les ids à un hashset
-		System.out.println("getting projects ids from mongo database");
-		try (MongoCursor<org.bson.Document> cursor = mongoClient.getDatabase(databaseName).getCollection(collectionName)
-				.find().projection(fields).iterator()) {
-			while (cursor.hasNext()) {
-				org.bson.Document document = cursor.next();
-				idsProjetsCrawles.add(document.getInteger("id"));
-			}
-		}
-	}
+        // on ajoute les ids à un hashset
+        System.out.println("getting projects ids from mongo database");
+        mongoClient = new MongoClient(new MongoClientURI(this.mongoURI));
+        try (MongoCursor<org.bson.Document> cursor = mongoClient.getDatabase(databaseName).getCollection(collectionName)
+                .find().projection(fields).iterator()) {
+            while (cursor.hasNext()) {
+                org.bson.Document document = cursor.next();
+                idsProjetsCrawles.add(document.getInteger("id"));
+            }
+        }
+        mongoClient.close();
+    }
 
-	private void doUpdate() throws IOException {
-		Elements scriptTags;
-		int nbPage = 1;
-		boolean running = true;
-		do {
-			String url = this.urlBase + nbPage;
-			System.out.println("scraping page : " + url);
-			Document doc;
-			doc = Jsoup.connect(url).get();
-			// scriptTags =
-			// doc.getElementsByClass("project-thumbnail-wrap");
-			scriptTags = doc.getElementsByAttribute("data-project_pid");
+    private void doUpdate() throws IOException {
+        Elements scriptTags;
+        int nbPage = 1;
+        boolean running = true;
+        do {
+            String url = this.urlBase + nbPage;
+            System.out.println("scraping page : " + url);
+            Document doc;
+            doc = Jsoup.connect(url).get();
+            // scriptTags =
+            // doc.getElementsByClass("project-thumbnail-wrap");
+            scriptTags = doc.getElementsByAttribute("data-project_pid");
 
-			if (scriptTags != null && !scriptTags.isEmpty()) {
-				// récupération du JSON contenant les infos de chaque projet
-				// Collection<JSONObject> collection = new ArrayList<>();
+            if (scriptTags != null && !scriptTags.isEmpty()) {
+                // récupération du JSON contenant les infos de chaque projet
+                // Collection<JSONObject> collection = new ArrayList<>();
 
-				for (int i = 0; i < scriptTags.size(); i++) {
-					Element element = scriptTags.get(i);
-					String urlProjet = "https://www.kickstarter.com"
-							+ element.getElementsByAttribute("href").attr("href");
-					int id = Integer.parseInt(element.attr("data-project_pid"));
+                for (int i = 0; i < scriptTags.size(); i++) {
+                    Element element = scriptTags.get(i);
+                    String urlProjet = "https://www.kickstarter.com"
+                            + element.getElementsByAttribute("href").attr("href");
+                    int id = Integer.parseInt(element.attr("data-project_pid"));
 
-					if (!idsProjetsCrawles.contains(id)) {
-						JSONObject jsonObject = buildJSONObject(urlProjet);
-						org.bson.Document document = org.bson.Document.parse(jsonObject.toString());
-						// document.put("_id", jsonObject.getInt("id"));
-						mongoDocuments.add(document);
-						mongoDocumentsJsonArray.put(jsonObject);
-					} else {
-						logger.info("skipping project " + urlProjet + " - " + id);
-						running = false;
-					}
-				}
-			} else {
-				running = false;
-			}
-			nbPage++;
-		} while (running);
-	}
+                    if (!idsProjetsCrawles.contains(id)) {
+                        JSONObject jsonObject = buildJSONObject(urlProjet);
+                        org.bson.Document document = org.bson.Document.parse(jsonObject.toString());
+                        // document.put("_id", jsonObject.getInt("id"));
+                        mongoDocuments.add(document);
+                        mongoDocumentsJsonArray.put(jsonObject);
+                    } else {
+                        logger.info("skipping project " + urlProjet + " - " + id);
+                        running = false;
+                    }
+                }
+            } else {
+                running = false;
+            }
+            nbPage++;
+        } while (running);
+    }
 
-	private JSONObject buildJSONObject(String url) throws IOException {
-		Document doc;
-		JSONObject jsonObject = null;
-		System.out.println("scraping project " + (++cptProjects) + " : " + url);
-		doc = Jsoup.connect(url).get();
-		Elements scriptTags = doc.getElementsByTag("script");
-		for (Element tag : scriptTags) {
-			for (DataNode node : tag.dataNodes()) {
-				BufferedReader reader = new BufferedReader(new StringReader(node.getWholeData()));
-				String line = null;
-				do {
-					line = reader.readLine();
-					if (line != null && line.startsWith("  window.current_project")) {
-						String jsonEncoded = line.substring(28, line.length() - 2);
-						String jsonDecoded = StringEscapeUtils.unescapeHtml4(jsonEncoded).replaceAll("\\\\\\\\",
-								"\\\\");
-						jsonObject = new JSONObject(jsonDecoded);
-					}
-				} while (line != null);
-				reader.close();
-			}
-		}
+    private JSONObject buildJSONObject(String url) throws IOException {
+        Document doc;
+        JSONObject jsonObject = null;
+        System.out.println("scraping project " + (++cptProjects) + " : " + url);
+        doc = Jsoup.connect(url).get();
+        Elements scriptTags = doc.getElementsByTag("script");
+        for (Element tag : scriptTags) {
+            for (DataNode node : tag.dataNodes()) {
+                BufferedReader reader = new BufferedReader(new StringReader(node.getWholeData()));
+                String line = null;
+                do {
+                    line = reader.readLine();
+                    if (line != null && line.startsWith("  window.current_project")) {
+                        String jsonEncoded = line.substring(28, line.length() - 2);
+                        String jsonDecoded = StringEscapeUtils.unescapeHtml4(jsonEncoded).replaceAll("\\\\\\\\",
+                                "\\\\");
+                        jsonObject = new JSONObject(jsonDecoded);
+                    }
+                } while (line != null);
+                reader.close();
+            }
+        }
 
-		return jsonObject;
-	}
+        return jsonObject;
+    }
 
-	private String getFormattedDate() {
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat format1 = new SimpleDateFormat("yyyy_MM_dd-kk_mm");
-		return format1.format(cal.getTime());
-	}
+    private String getFormattedDate() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy_MM_dd-kk_mm");
+        return format1.format(cal.getTime());
+    }
 }
